cd43d97ce9112a7540c69ecee45b92859b7a72b0
Julien <Julien@DESKTOP-3DU0OA5>
2021-11-07 01:54:10 +0100

    Added Joueur.java

---
+++ WitchHunt/src/Joueur.java
@@ -0,0 +1,13 @@
+
+public class Joueur {
+	private String nomJoueur;
+	private boolean estSorciere;
+	private int score;
+	private boolean elimine;
+	private boolean revelee;
+	
+	public Joueur(String nom) {
+		this.nomJoueur=nom
+	}
+	
+}
