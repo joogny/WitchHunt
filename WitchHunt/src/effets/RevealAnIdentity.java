30d0b2de3c74a9b07291b1da629122d7229ee4fd
Joogny <millourj@gmail.com>
2021-11-28 20:10:18 +0100

    Carte bloquante

--- WitchHunt/src/effets/RevealAnIdentity.java
+++ WitchHunt/src/effets/RevealAnIdentity.java
@@ -3,6 +3,7 @@ package effets;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import partie.Carte;
 import partie.Effet;
 import partie.Joueur;
 import partie.NoPlayersToChooseFromException;
@@ -10,8 +11,9 @@ import partie.Partie;
 
 public class RevealAnIdentity extends Effet {
 	private static final String enoncé = "Reveal another player's identity. \nWitch: You gain 2pts. You take next turn. \nVillager:You lose 2pts. They take next turn.";
+	private static final String nomCarteBloquante = "Broomstick";
 
-	public RevealAnIdentity() {
+	public RevealAnIdentity(Carte c) {
 		super(enoncé);
 		// TODO Auto-generated constructor stub
 	}
@@ -21,7 +23,19 @@ public class RevealAnIdentity extends Effet {
 	public void activerEffet(Joueur joueurCarte,Joueur accusateur) {
 		Joueur j;
 		try {
-			j = joueurCarte.choisirJoueurNonRevelee();
+			ArrayList<Joueur> joueurs = new ArrayList<>();
+			joueurs.addAll(Partie.getInstance().getListeJoueurs().getJoueursNonRevelées());
+			joueurs.remove(joueurCarte);
+			Iterator<Joueur> it = joueurs.iterator();
+			joueurs.remove(joueurCarte);
+			while(it.hasNext()) {
+				Iterator<Carte> itC = it.next().getRevealedCards().iterator();
+				if(itC.next().getNomCarte()==nomCarteBloquante) {
+					it.remove();
+				}
+			}
+			
+			j = joueurCarte.choisirJoueur(joueurs);
 			j.setEstRevele();
 			if(j.estSorciere()) {
 				joueurCarte.addToScore(2);
@@ -35,8 +49,7 @@ public class RevealAnIdentity extends Effet {
 				}
 			}
 		} catch (NoPlayersToChooseFromException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("You have no one to use this effect on! The only player remaining has the " + nomCarteBloquante + " card!");
 		}
 	}
 	
